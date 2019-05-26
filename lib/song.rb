7a2ece247fa77e98160659ef71e51a44eb746c4d
Stella McEwen <mcewen.stella@gmail.com>
2019-05-26 02:34:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,8 +12,7 @@ class Song
   end
   
   def self.new_by_filename(file)
-    song_info = file.chomp(".mp3").split(" - ")
-    song = self.new(song_info[1])
+    song = self.new(file.split(" - ")[1])
     song.artist = Artist.find_or_create_by_name(song_info[0])
     song
   end
